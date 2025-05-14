517f10e912d79c8a477fe6fca7ff54e56e2583d5
Johan Colorado Caicedo <johancol@ifarm2401.jlab.org>
2025-05-14 11:54:54 -0400

    Some modifications

--- pi0InvM.C
+++ pi0InvM.C
@@ -14,7 +14,7 @@
 #include "TCanvas.h"
 // for ifstream
 #include <fstream>
-
+#include <TRandom3.h>
 
 
 using namespace std;
@@ -23,9 +23,9 @@ using namespace std;
 
 void pi0InvM() {
     const Double_t z_calo = 8.14; // position of calorimeter from the target in m
-    const Double_t z_target = 0.0;    // position of target
-    const Double_t z_orign = 0.09 ;   // from the center of the target to the gamma gamma vertex
-    const Double_t vertex_z = z_calo - z_target - z_orign;  // position of vertex, where the pi0 is created, right in the middle of the target
+    const Double_t z_target = 0.09;    // position of target
+    //const Double_t z_origin = 0.0;
+    //const Double_t vertex_z = z_target - z_origin;  // position of vertex, where the pi0 is created, right in the middle of the target
 
     TChain *ch = new TChain("T");
 
@@ -59,20 +59,23 @@ void pi0InvM() {
     ch->SetBranchAddress("earm.ecal.clus.nblk", &clus_nblk);  
     ch->SetBranchAddress("earm.ecal.clus.eblk", &clus_eblk);
 
-    TH1F *h_pi0_mass = new TH1F("h_pi0_mass", "Pi0 Invariant Mass;M_{#pi^{0}} [GeV];Events", 100, 0, 0.6);
+    TH1F *h_pi0_mass = new TH1F("h_pi0_mass", "Pi0 Invariant Mass;M_{#pi^{0}} [GeV];Events", 80, 0, 0.6);
     TH2F *h_photons_time = new TH2F("h_photons_time", "Time of photons;earm.ecal.clus.atimeblk[0];earm.ecal.clus.atimeblk[1]", 100, 0, 540, 100, 0, 540);
     TH2F *h_xy = new TH2F("h_xy", "xy;earm.ecal.clus.x;earm.ecal.clus.y", 25, -1.5, 1.5, 25, -0.65, 0.6);
     TH1F *h_opening_angle = new TH1F("h_opening_angle", "Opening angle between #gamma#gamma [deg];Angle [deg];Events", 90, 0, 30);
     Long64_t nEvents = ch->GetEntries();
     cout << "Number of events: " << nEvents << endl;
 
+    gRandom->SetSeed(0);
     for (Long64_t i = 0; i < nEvents; i++) {
         ch->GetEntry(i);
         // Check that there are at least two clusters
         //h_xy->Fill(ecal_x[0], ecal_y[0]);
         //h_xy->Fill(ecal_x[1], ecal_y[1]);
         if (nclus != 2) continue;
-        if ((ecal_e[0] + ecal_e[1]) < 1.0) continue;
+        if ((ecal_e[0] + ecal_e[1]) < 0.5) continue;
+        //if (ecal_e[0] < 1) continue;
+        //if (ecal_e[1] < 0.2) continue;
         if (ecal_e[0] < 0.2 || ecal_e[1] < 0.2) continue;    // Minimum cluster energy cut Tune the 0.2 GeV threshold based on your noise level.
         if (clus_nblk[0] < 2 || clus_nblk[1] < 2) continue;  // Minimum number of blocks per cluster
         
@@ -89,7 +92,11 @@ void pi0InvM() {
         TVector3 pos1(ecal_x[0], ecal_y[0], z_calo);  // in m
         TVector3 pos2(ecal_x[1], ecal_y[1], z_calo);  // in m
 
-        TVector3 vertex(0, 0, z_target);
+        // uniform smearing ±15 cm around (0,0,0.09)
+        double vertex_x_smeared = gRandom->Uniform(-0.15, +0.15);
+        double vertex_y_smeared = gRandom->Uniform(-0.15, +0.15);
+        double vertex_z_smeared = z_target + gRandom->Uniform(-0.15, +0.15);
+        TVector3 vertex(vertex_x_smeared, vertex_y_smeared, vertex_z_smeared);
         TVector3 dir1 = (pos1 - vertex).Unit();
         TVector3 dir2 = (pos2 - vertex).Unit();
 
@@ -100,15 +107,15 @@ void pi0InvM() {
         Double_t pi0_mass = (ph1 + ph2).M();
 
         Double_t opening_angle = dir1.Angle(dir2) * (180.0 / TMath::Pi());
-        if (opening_angle < 4 || opening_angle > 7.5) continue;  // The lower cut (e.g., < 6°) removes nearly collinear photon pairs → likely merged.
+        if (opening_angle < 3.5 || opening_angle > 8) continue;  // The lower cut (e.g., < 6°) removes nearly collinear photon pairs → likely merged.
                                                                 // The upper cut (e.g., > 80°) removes highly unphysical, possibly misreconstructed pairs.
 
 
-        if ((ph1 + ph2).M() <= 0.4 && (ph1 + ph2).M() >= 0.06)
+        if ((ph1 + ph2).M() <= 0.4 && (ph1 + ph2).M() >= 0)
             { // cuts to ensure some quality and avoid bugs (inf, -nan)
                 //cuts in time
-                if (clus_a_time[0] > 180 && clus_a_time[0] < 220 && 
-                    clus_a_time[1] > 180 && clus_a_time[1] < 220 && 
+                if (clus_a_time[0] > 100 && clus_a_time[0] < 300 && 
+                    clus_a_time[1] > 100 && clus_a_time[1] < 300 && 
                     fabs(clus_a_time[0] - clus_a_time[1]) < 10) { // difference in time < 4 ns
                     h_pi0_mass->Fill(pi0_mass);
                     h_photons_time->Fill(clus_a_time[0], clus_a_time[1]);
