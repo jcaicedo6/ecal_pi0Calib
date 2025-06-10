458d956ef74de6d0973305af7a01e22c7ab0ea2d
Johan Colorado Caicedo <johancaicedo6@gmail.com>
2025-06-10 12:10:18 -0400

    refining some details

--- pi0InvM.C
+++ pi0InvM.C
@@ -32,8 +32,13 @@ void pi0InvM() {
     //string path = "/volatile/halla/sbs/sbs-gep/GEP_REPLAYS/GEP1/LH2/prod_realign_lowcur_April28/rootfiles/";
     //ifstream infile("lists_runs/runfiles_3173_to_3192.txt");
 
-    string path = "/adaqfs/home/a-onl/sbs/Rootfiles/";
-    ifstream infile("lists_runs/runfiles_3637.txt");
+    //string path = "/adaqfs/home/a-onl/sbs/Rootfiles/";
+    //ifstream infile("lists_runs/runfiles_3637.txt");
+
+    // For the Monte Carlo simulation data
+    string path = "/volatile/halla/sbs/efuchey/GEP3mod_pi0ECal_20250605/";
+    ifstream infile("lists_runs/replays_mc.txt");
+
     string filename;
 
     while (getline(infile, filename)) {
@@ -63,10 +68,14 @@ void pi0InvM() {
     ch->SetBranchAddress("earm.ecal.clus.nblk", &clus_nblk);  
     ch->SetBranchAddress("earm.ecal.clus.eblk", &clus_eblk);
 
-    TH1F *h_pi0_mass = new TH1F("h_pi0_mass", "Pi0 Invariant Mass;M_{#pi^{0}} [GeV];Events", 80, 0, 0.6);
+    Double_t goodblock_e[1000];
+    ch->SetBranchAddress("earm.ecal.goodblock.e", &goodblock_e);
+
+    TH1F *h_pi0_mass = new TH1F("h_pi0_mass", "Uncorrected #pi^{0} Invariant Mass;M_{#pi^{0}} [GeV];Events", 100, 0, 0.2);
     TH2F *h_photons_time = new TH2F("h_photons_time", "Time of photons;earm.ecal.clus.atimeblk[icl];earm.ecal.clus.atimeblk[jcl]", 100, 0, 540, 100, 0, 540);
     TH2F *h_xy = new TH2F("h_xy", "xy;earm.ecal.clus.x;earm.ecal.clus.y", 25, -1.5, 1.5, 25, -0.65, 0.6);
-    TH1F *h_opening_angle = new TH1F("h_opening_angle", "Opening angle between #gamma#gamma [deg];Angle [deg];Events", 90, 0, 30);
+    TH1F *h_opening_angle = new TH1F("h_opening_angle", "Opening angle between #gamma#gamma;Angle [deg];Events", 100, 0, 30);
+
     Long64_t nEvents = ch->GetEntries();
     cout << "Number of events: " << nEvents << endl;
 
@@ -91,8 +100,8 @@ void pi0InvM() {
                 if (clus_nblk[icl] < 2 || clus_nblk[jcl] < 2) continue;
                 Double_t deltaR = sqrt(pow(ecal_x[icl] - ecal_x[jcl], 2) + pow(ecal_y[icl] - ecal_y[jcl], 2));
                 if (deltaR < 0.07) continue;
-                if (clus_a_time[icl] < 100 || clus_a_time[icl] > 300) continue;
-                if (clus_a_time[jcl] < 100 || clus_a_time[jcl] > 300) continue;
+                //if (clus_a_time[icl] < 100 || clus_a_time[icl] > 140) continue;
+                //if (clus_a_time[jcl] < 100 || clus_a_time[jcl] > 140) continue;
                 double dt = fabs(clus_a_time[icl] - clus_a_time[jcl]);
                 if (dt < best_dt && dt < 4) { // time window 
                     best_dt = dt;
@@ -132,86 +141,52 @@ void pi0InvM() {
             Double_t pi0_mass = (ph1 + ph2).M();
 
             Double_t opening_angle = dir1.Angle(dir2) * (180.0 / TMath::Pi());
+            //if (opening_angle < 3) continue;
             if (opening_angle < 3.5 || opening_angle > 8) continue;  // The lower cut (e.g., < 6°) removes nearly collinear photon pairs → likely merged.
                                                                     // The upper cut (e.g., > 80°) removes highly unphysical, possibly misreconstructed pairs.
-            if (pi0_mass < 0 || pi0_mass > 0.4) continue;
+            //if (pi0_mass < 0 || pi0_mass > 0.4) continue;
             h_pi0_mass->Fill(pi0_mass);
             h_photons_time->Fill(clus_a_time[best_icl], clus_a_time[best_jcl]);
             h_opening_angle->Fill(opening_angle);
             h_xy->Fill(ecal_x[best_icl], ecal_y[best_icl]);
             h_xy->Fill(ecal_x[best_jcl], ecal_y[best_jcl]);
         
-        /*// Loop over all unique pairs of clusters
-        for (int icl = 0; icl < nclus; ++icl) {
-            for (int jcl = icl + 1; jcl < nclus; ++jcl) {
-                if ((ecal_e[icl] + ecal_e[jcl]) < 0.5) continue;
-                //if (ecal_e[icl] < 1) continue;
-                //if (ecal_e[jcl] < 0.2) continue;
-                if (ecal_e[icl] < 0.2 || ecal_e[jcl] < 0.2) continue;    // Minimum cluster energy cut Tune the 0.2 GeV threshold based on your noise level.
-                if (clus_nblk[icl] < 2 || clus_nblk[jcl] < 2) continue;  // Minimum number of blocks per cluster
-                
-                //if (clus_nblk[icl] < 2 || clus_nblk[jcl] < 2) continue; // Require at least 2 blocks per cluster
-                //if (clus_eblk[icl] < 0.05 || clus_eblk[jcl] < 0.05) continue; // Require significant energy in the highest-energy block
-
-                Double_t deltaR = sqrt(pow(ecal_x[icl] - ecal_x[jcl], 2) + pow(ecal_y[icl] - ecal_y[jcl], 2));
-                if (deltaR < 0.07) continue; // reject overlapping clusters
-
-                TVector3 pos1(ecal_x[icl], ecal_y[icl], z_calo);  // in m
-                TVector3 pos2(ecal_x[jcl], ecal_y[jcl], z_calo);  // in m
-
-                // uniform smearing ±15 cm around (0,0,0.09)
-                //double vertex_x_smeared = gRandom->Uniform(-0.15, +0.15);
-                //double vertex_y_smeared = gRandom->Uniform(-0.15, +0.15);
-                //double vertex_z_smeared = z_target + gRandom->Uniform(-0.15, +0.15);
-                //TVector3 vertex(vertex_x_smeared, vertex_y_smeared, vertex_z_smeared);
-                TVector3 vertex(0, 0, z_target);
-                TVector3 dir1 = (pos1 - vertex).Unit();
-                TVector3 dir2 = (pos2 - vertex).Unit();
-
-                TLorentzVector ph1(dir1.X() * ecal_e[icl], dir1.Y() * ecal_e[icl], dir1.Z() * ecal_e[icl], ecal_e[icl]);
-                TLorentzVector ph2(dir2.X() * ecal_e[jcl], dir2.Y() * ecal_e[jcl], dir2.Z() * ecal_e[jcl], ecal_e[jcl]);
-
-
-                Double_t pi0_mass = (ph1 + ph2).M();
-
-                Double_t opening_angle = dir1.Angle(dir2) * (180.0 / TMath::Pi());
-                if (opening_angle < 3.5 || opening_angle > 10) continue;  // The lower cut (e.g., < 6°) removes nearly collinear photon pairs → likely merged.
-                                                                        // The upper cut (e.g., > 80°) removes highly unphysical, possibly misreconstructed pairs.
-
-
-                if ((ph1 + ph2).M() <= 0.4 && (ph1 + ph2).M() >= 0){ 
-                    // cuts to ensure some quality and avoid bugs (inf, -nan)
-                    //cuts in time
-                    if (clus_a_time[icl] > 100 && clus_a_time[icl] < 300 && 
-                        clus_a_time[jcl] > 100 && clus_a_time[jcl] < 300 && 
-                        fabs(clus_a_time[icl] - clus_a_time[jcl]) < 10) { // difference in time < 4 ns
-                        h_pi0_mass->Fill(pi0_mass);
-                        h_photons_time->Fill(clus_a_time[icl], clus_a_time[jcl]);
-                        h_opening_angle->Fill(opening_angle);
-                        h_xy->Fill(ecal_x[icl], ecal_y[icl]);
-                        h_xy->Fill(ecal_x[jcl], ecal_y[jcl]);       
-                    }
-                        
-                }
-            }*/
         }    
         
     }
 
-    TCanvas *c_pi0InvM = new TCanvas("c_pi0InvM", "c_pi0InvM", 800, 600);
+    TCanvas *c_pi0InvM = new TCanvas("c_pi0InvM", "Uncorrected #pi^{0} Invariant Mass", 800, 600);
+    //h_pi0_mass->SetStats(0);
+    h_pi0_mass->GetXaxis()->SetTitle("M_{#pi^{0}} [GeV]");
+    h_pi0_mass->GetYaxis()->SetTitle("Events");
+    h_pi0_mass->SetLineColor(kBlue);
+    h_pi0_mass->SetMarkerStyle(20);
     h_pi0_mass->Draw();
+    //c_pi0InvM->SaveAs("plots/pi0_invariant_mass_before.png");
 
-    TCanvas *c_photons_time = new TCanvas("c_photons_time", "c_photons_time", 800, 600);
+    TCanvas *c_photons_time = new TCanvas("c_photons_time", "Time window", 800, 600);
+    h_photons_time->SetStats(0);
+    h_photons_time->GetXaxis()->SetTitle("t [ns]");
+    h_photons_time->GetYaxis()->SetTitle("t [ns]");
+    h_photons_time->SetTitle("Time of Photons");
     h_photons_time->Draw("colz");
+    //c_photons_time->SaveAs("plots/photons_time.png");
 
     TCanvas *c_xy = new TCanvas("c_xy", "c_xy", 800, 600);
     h_xy->SetStats(0);
     h_xy->Draw("colz");
+    h_xy->GetXaxis()->SetTitle("x [m]");
+    h_xy->GetYaxis()->SetTitle("y [m]");
+    //c_xy->SaveAs("plots/xy_clusters.png");
 
     TCanvas *c_opening = new TCanvas("c_opening", "Opening Angle", 800, 600);
+    h_opening_angle->SetStats(0);
+    h_opening_angle->GetXaxis()->SetTitle("Angle [deg]");
+    h_opening_angle->GetYaxis()->SetTitle("Events");
     h_opening_angle->Draw();
+    c_opening->SaveAs("plots/opening_angle.png");
 
-    TFile *f = new TFile("pi0_mass.root", "RECREATE");
+    TFile *f = new TFile("histos/pi0_mass.root", "RECREATE");
     h_pi0_mass->Write();
     f->Close();
 }
